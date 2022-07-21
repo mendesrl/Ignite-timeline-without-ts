fcc4049a46f6decf862c301f84347df36b6d58bc
mendesrl <ribeiro.larissamendes@gmail.com>
2022-07-21 17:58:38 -0400

    adicionando id aos componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,7 @@ export function Post({ author, publishedAt, content }) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
-    setNewCommentText("")
+    setNewCommentText("");
   }
   return (
     <article className={styles.post}>
@@ -53,10 +53,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content} </a>
               </p>
             );
@@ -81,7 +81,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
