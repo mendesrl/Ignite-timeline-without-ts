e1611b0d9df943dd59ff01bed8fe686cc9536ad8
mendesrl <ribeiro.larissamendes@gmail.com>
2022-07-25 23:37:02 -0400

    refatorando codigo

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,7 +11,9 @@ export function Comment({ content, deleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1)
+    setLikeCount(state=>{
+      return state + 1;
+    })
   }
 
   return (
